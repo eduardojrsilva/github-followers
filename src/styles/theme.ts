3d5b34d4cf1004edcc7e36a9ccd531e3736810a5
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-22 15:31:09 -0300

    adjusting theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,18 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  colors: {
-    black: '#000',
-  },
+  white: '#fff',
 
-  fonts: {
-    roboto: 'Roboto, sans-serif',
-  },
-};
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'blue-300': '#00AFC1',
+  'blue-500': '#0093AB',
+  'blue-700': '#006778',
+} as const;
